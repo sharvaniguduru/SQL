c5268803c628fed080d0a511b0838fb2f7b251f4
sharvani <sharvaniguduru3@gmail.com>
2026-06-22 23:11:27 -0400

    Weather app project

--- test/creation table.sql
+++
@@ -1,3 +0,0 @@
-use guduru;
-Drop table if exists grocery_sales;
-create table grocery_sales(order_id INT PRIMARY KEY,order_date DATE,customer_name VARCHAR(100),city VARCHAR(100),category VARCHAR(100),product VARCHAR(100),quantity INT,unit_price DECIMAL(10,2),payment_method VARCHAR(50));
--- test/insertion rows.sql
+++
@@ -1,8 +0,0 @@
-use guduru;
-
-INSERT INTO grocery_sales VALUES
-(1,'2024-01-05','Ravi','Dallas','Fruits','Apple',5,1.20,'Card'),
-(2,'2024-01-07','John','Austin','Vegetables','Tomato',3,0.80,'Cash'),
-(3,'2024-01-10','Anu','Dallas','Dairy','Milk',2,3.50,'Card'),
-(4,'2024-02-01','Sara','Houston','Snacks','Chips',4,2.00,'UPI'),
-(5,'2024-02-05','David','Austin','Fruits','Banana',6,0.50,'Cash');
